592fb83e343d41a1df8cb50bda114bac99500bc5
Andrei Marincas <andrei.marincas@solera.com>
2022-11-17 10:52:32 +0100

    Creatin the UI + model for User

---
+++ src/main/java/com/javaassessment/codingexercise/model/User.java
@@ -0,0 +1,32 @@
+package com.javaassessment.codingexercise.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+    public User(String firstName, String lastName, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+
+}
